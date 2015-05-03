2469b5c731523241030004845f6ee168f781fa1e
Lorena Mesa <me@lorenamesa.com>
2015-05-03 03:37:52 -0500

    added heroku postgres url for app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 # app.config.from_object('config')
 app.secret_key = 'this-really-should-be-changed'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/brownbabyreads'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/brownbabyreads'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_PURPLE_URL')
 db = SQLAlchemy(app)
 
 from app import views, models
